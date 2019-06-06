a6d181feb2cea5d0eec51b644e2037cc4f79a1e1
Thiago Bianeck <thiagobianeck@bitzsoftwares.com.br>
2019-06-06 13:45:08 -0300

    Deve inserir usuário com sucesso

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const app = require('express')();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.status(200).send();
 });
@@ -10,5 +14,9 @@ app.get('/users', (req, res) => {
     ];
     res.status(200).json(users);
 });
+
+app.post('/users', (req, res) => {
+    res.status(201).json(req.body);
+})
 
 module.exports = app;
