956dc9a443c1e57d358f430ddb2ba1724501df56
Thiago Bianeck <thiagobianeck@bitzsoftwares.com.br>
2019-06-07 13:30:29 -0300

    Devo separar camada de serviço

--- src/app.js
+++ src/app.js
@@ -10,13 +10,14 @@ app.db = knex(knexfile.test);
 // app.use(knexLogger(app.db));
 
 consign({ cwd: 'src', verbose: false })
-.include('./config/middlewares.js')
-.then('./routes')
-.then('./config/routes.js')
-.into(app);
+  .include('./config/middlewares.js')
+  .then('./services')
+  .then('./routes')
+  .then('./config/routes.js')
+  .into(app);
 
 app.get('/', (req, res) => {
-    res.status(200).send();
+  res.status(200).send();
 });
 
 // app.db.on('query', (query) => {
