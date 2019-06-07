d97afdf41d3e0ef6e0d8ceffe3952261198c5ed6
Thiago Bianeck <thiagobianeck@bitzsoftwares.com.br>
2019-06-07 16:04:07 -0300

    Não deve inserir usuário com e-mail já existente

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+const mail = `${Date.now()}@mail.com`;
+
 test('Deve listar todos os usuários', () => {
   return  request(app).get('/users')
   .then(res => {
@@ -11,13 +13,12 @@ test('Deve listar todos os usuários', () => {
 });
 
 test('Deve inserir usuário com sucesso ', () => {
-  const mail = `${Date.now()}@mail.com`;
   return request(app).post('/users')
     .send({ name: "Walter Mitty", mail, passwd: "123456"})
     .then((res) => {
       expect(res.status).toBe(201);
       expect(res.body.name).toBe('Walter Mitty');
-    })
+    });
 });
 
 test('Não deve inserir usuário sem nome', () => {
@@ -45,4 +46,13 @@ test('Não deve inserir usuário sem senha', (done) => {
       done();
     })
     .catch(err => done.fail(err));
+});
+
+test('Não deve inserir usuário com email existente', () => {
+  return request(app).post('/users')
+    .send({ name: "Walter Mitty", mail, passwd: "123456"})
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Já existe um usuário com este email');
+    });
 });
